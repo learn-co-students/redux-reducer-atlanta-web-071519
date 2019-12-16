b86447f75b29929dc89b4b268a9167c3666ad5e5
jonathanboss17 <jtboss47@gmail.com>
2019-12-16 00:24:57 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,11 +1,13 @@
+let state = {friends: []}
+
 export function manageFriends(state, action){
-    switch(action.type){
+    switch (action.type) {
         case 'ADD_FRIEND': {
-            return { friends: [...state.friends, action.friend]}
+            return {friends: [...state.friends, action.friend]}
         }
         case 'REMOVE_FRIEND': {
             let x = [...state.friends].filter(friend => friend.id !== action.id)
-            return { friends: x}
+            return {friends: x}
         }
         default: {
             return state
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,12 +1,12 @@
-let numberOfPresents = 0; 
+let state = {numberOfPresents: 0};
 
-export function managePresents(state = numberOfPresents, action){
-    switch(action.type){
+export function managePresents(state, action){
+    switch(action.type) {
         case 'INCREASE': {
-            return { numberOfPresents: state.numberOfPresents + 1}; 
+            return {numberOfPresents: state.numberOfPresents + 1}
         }
         default: {
-            return state; 
+            return state
         }
     }
 }
